c53b6a40c3996479c1d13cd04f496ee794be8df9
supershadoe <supershadoe@proton.me>
2022-07-05 21:46:46 +0530

    Better Playtile service
    Feels a bit faster with less bloated code idk probably
    placebo

--- src/main/kotlin/PlayTile.kt
+++ src/main/kotlin/PlayTile.kt
@@ -10,6 +10,7 @@ import android.content.Context
 import android.media.AudioAttributes
 import android.media.AudioFocusRequest
 import android.media.AudioManager
+import android.media.AudioManager.OnAudioFocusChangeListener as AFCListener
 import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.PowerManager
@@ -24,181 +25,116 @@ import android.service.quicksettings.TileService
  * Drawbacks are app can't be used by users who own Android devices with android version <7.0
  * And quick setting tile maybe useless for people who use this app once in a blue moon.
  */
-class PlayTile: TileService(), AudioManager.OnAudioFocusChangeListener {
+class PlayTile: TileService() {
 
-    // Declaration of various variables
-    // enum class to show different states of audio focus
-    enum class AudioFocus {
-        NoFocusNoDuck,
-        NoFocusCanDuck,
-        Focused
-    }
     // Variable storing the current audio focus state
-    private var audioFocus = AudioFocus.NoFocusNoDuck
+    private var isAudioFocused = false
+    private var audioFocusChangeListener = AFCListener {
+        when(it) {
+            AudioManager.AUDIOFOCUS_GAIN ->
+                playbackThread.audioTrack.setVolume(1.0f)
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK ->
+                playbackThread.audioTrack.setVolume(0.1f)
+            AudioManager.AUDIOFOCUS_LOSS,
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ->
+                onClick()
+        }
+    }
     // Instance of PlaybackThread class
-    private var playbackThread: PlaybackThread? = null
+    private lateinit var playbackThread: PlaybackThread
     // Declaring audioManager, wakeLock and wifiLock to use them in future
     private lateinit var audioManager: AudioManager
     private lateinit var wakeLock: PowerManager.WakeLock
     private lateinit var wifiLock: WifiManager.WifiLock
 
     /**
-     * manageAudioFocus function: Used to create a AudioFocusRequest instance to get AudioFocus for
-     *                            app and also to declare onAudioFocusChange() function as
-     *                            AudioFocusChangeListener
-     * @param getOrGiveUp(Boolean) AudioFocus is gained or given up if the value is true or false respectively
+     * manageAudioFocus function: Creates an AudioFocusRequest for playing the audio
+     * @param toFocus(Boolean) Flag to decide whether audioFocus should be requested or given up.
      */
-    @Suppress("DEPRECATION")
-    private fun manageAudioFocus(getOrGiveUp: Boolean) {
-        var audioFocusRequest: AudioFocusRequest? = null
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val audioAttributes = AudioAttributes.Builder()
+    private fun createAudioFocusReq(toFocus: Boolean) {
+        audioManager.run {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val audioAttributes = AudioAttributes.Builder()
                     .setUsage(AudioAttributes.USAGE_MEDIA)
                     .setLegacyStreamType(AudioManager.STREAM_MUSIC)
                     .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
                     .build()
-            audioFocusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+                val audioFocusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
                     .setAudioAttributes(audioAttributes)
                     .setAcceptsDelayedFocusGain(false)
                     .setWillPauseWhenDucked(true)
-                    .setOnAudioFocusChangeListener(this)
+                    .setOnAudioFocusChangeListener(audioFocusChangeListener)
                     .build()
+                audioFocusRequest?.let {
+                    if (toFocus) requestAudioFocus(it)
+                    else abandonAudioFocusRequest(it)
+                }
+            } else {
+                @Suppress("DEPRECATION")
+                if(toFocus) requestAudioFocus(
+                    audioFocusChangeListener,
+                    AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN
+                )
+                else abandonAudioFocus(audioFocusChangeListener)
+            }
         }
-        if (getOrGiveUp)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                // Gain AudioFocus(For Android version >= 8.0 (Oreo)
-                if (audioFocus != AudioFocus.Focused && audioFocusRequest != null &&
-                        AudioManager.AUDIOFOCUS_REQUEST_GRANTED == audioManager.requestAudioFocus(audioFocusRequest)) {
-                    audioFocus = AudioFocus.Focused
-                }
-            } else {
-                // Gain AudioFocus(For Android version < 8.0 (Oreo)
-                if (audioFocus != AudioFocus.Focused &&
-                        AudioManager.AUDIOFOCUS_REQUEST_GRANTED == audioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN)) {
-                    audioFocus = AudioFocus.Focused
-                }
-            }
-        else
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                // Give up AudioFocus(For Android version >= 8.0 (Oreo)
-                if (audioFocus == AudioFocus.Focused && audioFocusRequest != null &&
-                        AudioManager.AUDIOFOCUS_REQUEST_GRANTED == audioManager.abandonAudioFocusRequest(audioFocusRequest)) {
-                    audioFocus = AudioFocus.NoFocusNoDuck
-                }
-            } else {
-                // Give up AudioFocus(For Android version < 8.0 (Oreo)
-                if (audioFocus == AudioFocus.Focused &&
-                        AudioManager.AUDIOFOCUS_REQUEST_GRANTED == audioManager.abandonAudioFocus(this)) {
-                    audioFocus = AudioFocus.NoFocusNoDuck
-                }
-            }
+            ?.takeIf { it == AudioManager.AUDIOFOCUS_REQUEST_GRANTED }
+            ?.also { isAudioFocused = toFocus }
     }
 
     /**
-     * processPlayRequest function: Starts playback when the quick settings tile is tapped
-     */
-    @SuppressLint("WakelockTimeout")
-    private fun processPlayRequest() {
-        // Get preference values to set up stream
-        val ipAddress = "0.0.0.0"
-        val port = 8000
-        val sampleRate = 48000
-        val stereo = true
-        // Gain audio focus
-        manageAudioFocus(getOrGiveUp = true)
-        // Initialize playback thread
-        playbackThread = PlaybackThread(ipAddress, port, sampleRate, stereo, audioManager)
-        // Start the thread
-        Thread(playbackThread).start()
-        // Acquire wake lock and wifi lock
-        wakeLock.acquire()
-        wifiLock.acquire()
-        // Set volume according to AudioFocus
-        setVolume(audioFocus)
-        // Set quick setting tile state to active and update tile
-        qsTile.state = Tile.STATE_ACTIVE
-        qsTile.label = ipAddress
-        qsTile.updateTile()
-    }
-
-    /**
-     * processStopRequest function: Stops playback when the quick settings tile is tapped
-     */
-    private fun processStopRequest() {
-        // Give up audio focus
-        manageAudioFocus(getOrGiveUp = false)
-        // Terminate playback thread
-        playbackThread?.terminate()
-        playbackThread = null
-        // Release wake lock and wifi lock if they are held
-        if(wakeLock.isHeld) wakeLock.release()
-        if(wifiLock.isHeld) wifiLock.release()
-        // Set quick setting tile state to inactive and update tile
-        qsTile.state = Tile.STATE_INACTIVE
-        qsTile.label = getString(R.string.qs_label)
-        qsTile.updateTile()
-    }
-
-    /**
-     * onCreate function: Executed when the quick setting tile is created
+     * Executed when the quick setting tile is created
      */
     override fun onCreate() {
         super.onCreate()
-        // Initializing audioManager, wakeLock and wifiLock
         audioManager = applicationContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
         wakeLock = (applicationContext.getSystemService(Context.POWER_SERVICE) as PowerManager)
                 .newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "onpa::wakeLock")
         wifiLock = (applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager)
                 .createWifiLock(WifiManager.WIFI_MODE_FULL_HIGH_PERF, "onpa::wifiLock")
-    }
-
-    /**
-     * onStartListening function: Executed when the quick setting tile comes in view and is ready to
-     *                            listen for events(touch, etc.)
-     */
-    override fun onStartListening() {
-        super.onStartListening()
-        if(qsTile != null && qsTile.label == getString(R.string.qs_label)) {
-            qsTile.state = Tile.STATE_INACTIVE
-            qsTile.updateTile()
+        qsTile?.takeIf {
+            it.label == getString(R.string.qs_label)
+        }?.apply {
+            state = Tile.STATE_INACTIVE
+            updateTile()
         }
     }
 
     /**
-     * onClick function: Executed when the quick setting tile is executed.
-     * App decides whether to play/pause playback according to the state of the quick setting tile at that moment.
+     * Executed when the quick setting tile is tapped.
+     * (or when audio focus is lost)
      */
     override fun onClick() {
         super.onClick()
-        when(qsTile.state) {
-            Tile.STATE_INACTIVE -> processPlayRequest()
-            Tile.STATE_ACTIVE -> processStopRequest()
-        }
-    }
+        @SuppressLint("WakelockTimeout")
+        when(qsTile?.state) {
+            Tile.STATE_INACTIVE -> {
+                createAudioFocusReq(true)
+                playbackThread = PlaybackThread(audioManager)
+                Thread(playbackThread).start()
 
-    /**
-     * setVolume function: Sets the volume of audio track according to AudioFocus
-     * @param audioFocus(AudioFocus) Tells what is the current AudioFocus status
-     */
-    private fun setVolume(audioFocus: AudioFocus) {
-        when (audioFocus) {
-            AudioFocus.NoFocusNoDuck -> processStopRequest()
-            AudioFocus.NoFocusCanDuck -> playbackThread!!.audioTrack.setVolume(0.1f)
-            else -> playbackThread!!.audioTrack.setVolume(1.0f)
-        }
-    }
+                wakeLock.acquire()
+                wifiLock.acquire()
 
-    /**
-     * onAudioFocusChange function: AudioFocusChangeListener which executes setVolume whenever
-     *                              AudioFocus changes to set volume appropriate for the situation
-     * @param focusChange(Int) The changed AudioFocus
-     */
-    override fun onAudioFocusChange(focusChange: Int) {
-        when(focusChange) {
-            AudioManager.AUDIOFOCUS_GAIN -> setVolume(AudioFocus.Focused)
-            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> setVolume(AudioFocus.NoFocusCanDuck)
-            AudioManager.AUDIOFOCUS_LOSS, AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ->
-                setVolume(AudioFocus.NoFocusNoDuck)
+                with(qsTile!!) {
+                    state = Tile.STATE_ACTIVE
+                    label = playbackThread.ipAddress
+                    updateTile()
+                }
+            }
+            Tile.STATE_ACTIVE -> {
+                createAudioFocusReq(false)
+                playbackThread.terminate()
+
+                if(wakeLock.isHeld) wakeLock.release()
+                if(wifiLock.isHeld) wifiLock.release()
+
+                with(qsTile!!) {
+                    state = Tile.STATE_INACTIVE
+                    label = getString(R.string.qs_label)
+                    updateTile()
+                }
+            }
         }
     }
 }
